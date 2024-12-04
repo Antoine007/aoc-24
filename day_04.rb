f2530f245efb47c0debeca4848a7a751ca675e69
Antoine <mr.magny+github@gmail.com>
2024-12-04 13:41:39 +0000

    day 4

--- day_04.rb
+++ day_04.rb
@@ -1,52 +1,63 @@
 def step1(example = false)
-  cards = []
+  count = 0
+  grid = []
+  # row 
   input(example).each do |line|
-    card = 0
-    x = line.split(' | ')
-    my_numbers = x[0].split(':')[1].split(' ').map(&:to_i)
-    draw = x[1].split(' ').map(&:to_i)
-    # p my_numbers
-    # p draw
-    my_numbers.each do |number|
-      if draw.include?(number)
-        card == 0 ? card += 1 : card *= 2
+    count += line.scan(/XMAS/).count
+    count += line.reverse.scan(/XMAS/).count
+    grid << line.strip.chars
+  end
+  # vertical
+  vertical_grid = grid.transpose
+  vertical_grid.each do |line|
+    count += line.join("").scan(/XMAS/).count
+    count += line.reverse.join("").scan(/XMAS/).count
+  end
+
+  reversed_grid = grid.reverse
+  x_rev = grid.map{ |row| row.reverse }
+  xy_rev = reversed_grid.map{ |row| row.reverse }
+
+  # diagonal 
+  [grid,reversed_grid, x_rev, xy_rev].each do |g|
+    g.each_with_index do |row, index|
+      row.each_with_index do |char, char_index|
+        next if g[index + 3] == nil
+        next if g[index + 3][char_index + 3] == nil
+
+        if char == "X" && g[index+1][char_index+1] == "M" && g[index+2][char_index+2] == "A" && g[index+3][char_index+3] == "S"
+          count += 1 
+        end
       end
     end
-    cards << card
   end
-  p cards
-  p cards.sum
+  p count
 end
 
 def step2(example = false)
-  rough_data = input(example).map do |card|
-    card.split(/[:|]/)
+  count = 0
+  grid = []
+  input(example).each do |line|
+    grid << line.strip.chars
   end
+  grid.each_with_index do |row, index|
+    row.each_with_index do |char, char_index|
+      next if index == 0 || index == grid.length - 1
+      next if char_index == 0 || char_index == row.length - 1
+      next unless char == "A"
 
-  # p rough_data
-  data = rough_data.map.with_index do |card, index|
-    {id: index+1, winning_numbers: card[1].split(" ").map(&:to_i), my_numbers: card[2].split(" ").map(&:to_i), w_count: 0}
-  end
-
-  # find number of winning numbers
-  data.each do |card|
-    card[:my_numbers].each do |my_number|
-      card[:w_count] += 1 if card[:winning_numbers].include?(my_number)
-    end
-  end
-
-  # p data
-  data.each do |card|
-    if card[:w_count] > 0
-      # index of copies are
-      copies_index = (card[:id]+1)..(card[:id]+card[:w_count])
-      # add the copies
-      copies_index.each do |index|
-        data << data[index-1]
+      if grid[index - 1][char_index - 1] == "M" && grid[index - 1][char_index + 1] == "M" && grid[index + 1][char_index + 1] == "S" && grid[index + 1][char_index - 1] == "S"
+        count += 1 
+      elsif grid[index + 1][char_index - 1] == "M" && grid[index + 1][char_index + 1] == "M" && grid[index - 1][char_index + 1] == "S" && grid[index - 1][char_index - 1] == "S"
+        count += 1
+      elsif grid[index - 1][char_index + 1] == "M" && grid[index + 1][char_index + 1] == "M" && grid[index + 1][char_index - 1] == "S" && grid[index - 1][char_index - 1] == "S"
+        count += 1
+      elsif grid[index - 1][char_index - 1] == "M" && grid[index + 1][char_index - 1] == "M"  && grid[index + 1][char_index + 1] == "S" && grid[index - 1][char_index + 1] == "S"
+        count += 1
       end
     end
   end
-  p data.count
+  p count
 end
 
 
