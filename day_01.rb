1f79a967970d47e54ef693c65959aafe1ec5646a
Antoine <mr.magny+github@gmail.com>
2024-12-04 10:02:17 +0000

    day 1

--- day_01.rb
+++ day_01.rb
@@ -1,47 +1,33 @@
 def step1(example = false)
-  numbers = []
-  input(example).each do |line|
-    number = ""
-    number << line.gsub(/[^0-9]/, '')[0]
-    number << line.reverse.gsub(/[^0-9]/, '')[0]
-    numbers << number.to_i
+  l1 = []
+  l2 = []
+  total = 0
+  input(example).each do |line| 
+    l1 << line.split[0].to_i
+    l2 << line.split[1].to_i
   end
-  # p numbers
-  p numbers.sum
+  l1 = l1.sort  
+  l2 = l2.sort
+
+  l1.each_with_index do |x, i|
+    total += (x - l2[i]).abs
+  end
+  p total
 end
 
-NUMBERS = {
-  'one' => 1,
-  'two' => 2,
-  'three' => 3,
-  'four' => 4,
-  'five' => 5,
-  'six' => 6,
-  'seven' => 7,
-  'eight' => 8,
-  'nine' => 9
-}
 
 def step2(example = false)
-  numbers = []
-  input(example).each do |line|
-    number = ""
-    ar = line.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/).flatten
-    if NUMBERS[ar[0]]
-      number << NUMBERS[ar[0]].to_s
-    else
-      number << ar[0].to_s
-    end
-
-    if NUMBERS[ar[-1]]
-      number << NUMBERS[ar[-1]].to_s
-    else
-      number << ar[-1].to_s
-    end
-    numbers << number.to_i
+  l1 = []
+  l2 = []
+  total = 0
+  input(example).each do |line| 
+    l1 << line.split[0].to_i
+    l2 << line.split[1].to_i
   end
-  # p numbers
-  p numbers.sum
+  l1.each do |x|
+    total += (l2.count(x) * x)
+  end
+  p total
 end
 
 def input(example)
