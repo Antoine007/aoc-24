30f61855786f082f0a8ed5214bd36db58426468b
Antoine <mr.magny+github@gmail.com>
2024-12-06 15:16:10 +0000

    day 6

--- day_06.rb
+++ day_06.rb
@@ -1,35 +1,139 @@
 def step1(example = false)
-  values = Array.new(input(example).length, [])
-  input(example).each_with_index do |line,index|
-    line.split(":")[1].strip.split(' ').each_with_index do |number,i|
-      new_v = values[i] ? [values[i], number.to_i].flatten : [number.to_i]
-      values[i] = new_v
-    end
+  map = []
+  direction = "n"
+  guard = [nil,nil]
+  input(example).each_with_index do |line, index|
+    map << line.strip.split('')
+    guard = [line.strip.index("^"), index] if line.strip.include?("^")
+  end
+  p guard
+  # p map
+  while guard[0] > 0 && guard[0] < map.length && guard[1] > 0 && guard[1] < map[0].length
+    direction = move(guard, direction, map)
+    # map.each do |line|
+    #   p line.join
+    # end
   end
 
-  min_time = []
-  values.each do |time, distance|
-    distances = []
-    (0..time).each_with_index do |pressed_x_seconds|
-      distances << pressed_x_seconds * (time - pressed_x_seconds)
-    end
-    min_time << distances.select{|x| x > distance}.count
+  count = 0
+  map.each do |line|
+    count += line.count("X")
   end
-  p min_time.inject(:*)
+  p count
 end
 
 def step2(example = false)
-  time = input(example)[0].split(":")[1].gsub(" ", "").to_i
-  distance = input(example)[1].split(":")[1].gsub(" ", "").to_i
-  distances = []
-  (0..time).each_with_index do |pressed_x_seconds|
-    distances << pressed_x_seconds * (time - pressed_x_seconds)
+  map = []
+  direction = "n"
+  guard = [nil,nil]
+  input(example).each_with_index do |line, index|
+    map << line.strip.split('')
+    guard = [line.strip.index("^"), index] if line.strip.include?("^")
+  end
+  # p guard
+  # p map
+  obstacles = [] 
+  map.each_with_index do |line, index|
+    line.each_with_index.select { |c, i| obstacles << [i,index] if c == "." }
+  end
+  # p obstacles
+  valid = []
+  obstacles.each do |obstacle|
+    new_map = Marshal.load(Marshal.dump(map))
+    new_map[obstacle[1]][obstacle[0]] = "#"
+    # Reset guard position for each iteration
+    current_guard = guard.dup
+    visited = Set.new
+    # p current_guard
+    # p direction
+
+    while current_guard[0] > 0 && current_guard[0] < map.length && current_guard[1] > 0 && current_guard[1] < map[0].length
+      state = [current_guard[0], current_guard[1], direction]
+      if visited.include?(state)
+        # p obstacle
+        valid << obstacle
+        break
+      end
+      visited.add(state)
+
+      direction = move2(current_guard, direction, new_map)
+    end
+    direction = "n"
   end
 
-  p distances.select{|x| x > distance}.count
+  p valid.count
 end
 
 
 def input(example)
   example == 'example' ? @input_example : @input
+end
+
+def move(guard, direction, map)  
+  case direction
+  when "n"
+    if map[guard[1] - 1] && map[guard[1] - 1][guard[0]] == "#"
+      "e"
+    else
+      map[guard[1]][guard[0]] = "X"
+      guard[1] -= 1
+      "n"
+    end
+  when "s"
+    if map[guard[1] + 1] && map[guard[1] + 1][guard[0]] == "#"
+      "w"
+    else
+      map[guard[1]][guard[0]] = "X"
+      guard[1] += 1
+      "s"
+    end
+  when "e"
+    if map[guard[0]+1] && map[guard[1]][guard[0] + 1] == "#"
+      "s"
+    else
+      map[guard[1]][guard[0]] = "X"
+      guard[0] += 1
+      "e"
+    end
+  when "w"
+    if map[guard[0]-1] && map[guard[1]][guard[0] - 1] == "#"
+      "n"
+    else
+      map[guard[1]][guard[0]] = "X"
+      guard[0] -= 1
+      "w"
+    end
+  end
+end
+def move2(guard, direction, map)  
+  case direction
+  when "n"
+    if map[guard[1] - 1] && map[guard[1] - 1][guard[0]] == "#"
+      "e"
+    else
+      guard[1] -= 1
+      "n"
+    end
+  when "s"
+    if map[guard[1] + 1] && map[guard[1] + 1][guard[0]] == "#"
+      "w"
+    else
+      guard[1] += 1
+      "s"
+    end
+  when "e"
+    if map[guard[0]+1] && map[guard[1]][guard[0] + 1] == "#"
+      "s"
+    else
+      guard[0] += 1
+      "e"
+    end
+  when "w"
+    if map[guard[0]-1] && map[guard[1]][guard[0] - 1] == "#"
+      "n"
+    else
+      guard[0] -= 1
+      "w"
+    end
+  end
 end
