aacad1fe0403cac4442b1ffcb9ae0211d3a225ae
Antoine <mr.magny+github@gmail.com>
2024-12-13 16:51:08 +0000

    day 13

--- day_13.rb
+++ day_13.rb
@@ -1,11 +1,79 @@
 def step1(example = false)
+  lines = []
   input(example).each do |line|
+    next if line.strip.empty?
     puts line
+    if line[7] == "A" 
+      lines << ["A",line[12..13].to_i,line[18..19].to_i]
+    elsif line[7] == "B"
+      lines << ["B",line[12..13].to_i,line[18..19].to_i]
+    else
+      lines << ["P",line.split(" ")[1][2..-1].to_i,line.split(" ")[2][2..-1].to_i]
+    end
   end
+  eqs = []
+  lines.reverse.each_slice(3) do |ls|
+    eqs << [ls[0][1],ls[0][2],ls[1][1],ls[1][2],ls[2][1],ls[2][2]]
+  end
+  eqs = eqs.reverse
+
+  count = 0
+  final = []
+  eqs.each do |eq|
+    a,b,bx,by,ax,ay = eq
+    valids = []
+    (0..100).each do |i|
+      (0..100).each do |y|
+        if [a,b] == [(ax * i) + (bx * y), (ay * i) + (by * y)]
+          valids << [eq,i,y]
+        end
+      end
+    end
+    p valids
+    final << valids
+  end
+  final.flatten(1).each do |f|
+    count += (f[1]*3) + f[2]
+  end
+  # p count
 end
 
 def step2(example = false)
-
+  lines = []
+  input(example).each do |line|
+    next if line.strip.empty?
+    # puts line
+    if line[7] == "A" 
+      lines << ["A",line[12..13].to_i,line[18..19].to_i]
+    elsif line[7] == "B"
+      lines << ["B",line[12..13].to_i,line[18..19].to_i]
+    else
+      lines << ["P",line.split(" ")[1][2..-1].to_i + 10000000000000,line.split(" ")[2][2..-1].to_i + 10000000000000]
+    end
+  end
+  eqs = []
+  lines.reverse.each_slice(3) do |ls|
+    eqs << [ls[0][1],ls[0][2],ls[1][1],ls[1][2],ls[2][1],ls[2][2]]
+  end
+  eqs = eqs.reverse
+  # p eqs
+  count = 0
+  final = []
+  eqs.each do |eq|
+    a,b,bx,by,ax,ay = eq
+    valids = []
+    # p [ax,ay,bx,by,a,b]
+    solution = solve_linear_system(ax, ay, bx, by, a, b)
+    if solution
+      valids << [eq, solution[0], solution[1]]
+    end
+    # p valids.count
+    final << valids
+  end
+  final.flatten(1).each do |f|
+    count += (f[1]*3) + f[2]
+  end
+  p count
 end
 
 
@@ -13,144 +81,28 @@ def input(example)
   example == 'example' ? @input_example : @input
 end
 
-require 'numo/narray'
 
-class Day13 < AOC
-  def solve(part:)
-    data = read_input_file
-    if part == 1
-      solve_part_one(data)
-    else
-      solve_part_two(data)
-    end
+# Given equations:
+# a = ax*i + bx*y
+# b = ay*i + by*y
+
+# We can rewrite this as a matrix equation:
+# [ax bx] [i] = [a]
+# [ay by] [y]   [b]
+
+def solve_linear_system(ax, ay, bx, by, a, b)
+  # Using Cramer's rule to solve the system
+  determinant = (ax * by) - (bx * ay)
+  return nil if determinant == 0 # No unique solution
+
+  # Solve for i and y
+  i = ((a * by) - (b * bx)) / determinant.to_f
+  y = ((ax * b) - (ay * a)) / determinant.to_f
+  p [i,y]
+  # Check if solutions are non-negative integers
+  if i >= 0 && y >= 0 && i == i.to_i && y == y.to_i
+    return [i.to_i, y.to_i]
   end
-
-  def solve_part_one(data)
-    result = 0
-    input =  IO.read(@input_file).split("\n\n")
-
-    input.each do |pattern|
-      pattern = pattern.split
-      result += 100 * find_mirror(pattern)[0]
-      pattern = pattern.map(&:chars).transpose.map(&:join)
-      result += find_mirror(pattern)[0]
-    end
-
-    result
-  end
-
-  def find_mirror(pattern)
-    result = []
-    (1...pattern.length).each do |i|
-      lines_to_check = [i, pattern.length - i].min
-      j = 0
-      while j < lines_to_check do
-        break if pattern[i - j - 1] != pattern[i + j]
-        j += 1
-      end
-      result << i if j == lines_to_check
-    end
-    result.empty? ? [0] : result
-  end
-
-  def solve_part_two(data)
-    input = data.map(&:strip).map(&:chars)
-
-    patterns = []
-    lines = []
-    input.each do |line|
-      if line.empty?
-        patterns << Pattern.new(lines)
-        lines = []
-      else
-        lines << line
-      end
-    end
-    patterns << Pattern.new(lines)
-
-    patterns.sum(&:score)
-  end
-
-  class Pattern
-    attr_accessor :map
-
-    def initialize(input)
-      @height = input.size
-      @width = input[0].size
-      @map = Numo::UInt8.zeros(@height, @width)
-      @height.times do |y|
-        @width.times do |x|
-          @map[y, x] = 1 if input[y][x] == '#'
-        end
-      end
-    end
-
-    def horizontal_reflection_defects(candidate_row)
-      return -1 if candidate_row <= 0 or candidate_row >= @height
-
-      defects = 0
-      1.upto([candidate_row, @height - candidate_row].min) do |check_offset|
-        @width.times do |x|
-          if @map[candidate_row + check_offset - 1, x] !=
-            @map[candidate_row - check_offset, x]
-            defects += 1
-          end
-        end
-      end
-      defects
-    end
-
-    def vertical_reflection_defects(candidate_col)
-      return -1 if candidate_col <= 0 or candidate_col >= @width
-
-      defects = 0
-      1.upto([candidate_col, @width - candidate_col].min) do |check_offset|
-        @height.times do |y|
-          if @map[y, candidate_col + check_offset - 1] !=
-            @map[y, candidate_col - check_offset]
-            defects += 1
-          end
-        end
-      end
-      defects
-    end
-
-    def score_horizontal_reflection
-      1.upto(@height - 1) do |candidate_row|
-        if horizontal_reflection_defects(candidate_row) == 1
-          return 100 * candidate_row
-        end
-      end
-      0
-    end
-
-    def score_vertical_reflection
-      1.upto(@width - 1) do |candidate_col|
-        if vertical_reflection_defects(candidate_col) == 1
-          return candidate_col
-        end
-      end
-      0
-    end
-
-    def score
-      score_horizontal_reflection + score_vertical_reflection
-    end
-
-    def inspect
-      to_s
-    end
-
-    def to_s
-      s = "<#{self.class}:\n"
-      @height.times do |y|
-        @width.times do |x|
-          s += @map[y, x].to_s
-        end
-        s += "\n"
-      end
-      s += ">"
-    end
-  end
-
+  
+  nil
 end
